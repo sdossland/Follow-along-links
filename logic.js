087c3dc02552a12149f2b83ae53a591e39eac8da
Sophia Dossland <sophia.dossland@gmail.com>
2017-04-25 20:12:21 -0700

    initiate HTML / content

--- logic.js
+++ logic.js
@@ -3,4 +3,9 @@
  */
 document.addEventListener("DOMContentLoaded", function () {
 
+  const links = document.querySelectorAll('a');
+  const highlight = document.createElement('span');
+
+  //add CSS class highlight to newly created span tag called 'highlight'
+  highlight.classList.add('highlight');
 });
